4353d36f9cfa9246fb9fd35c3415e15cb7a3bc38
Fabien Cortina <fabien.cortina@gmail.com>
2018-12-27 13:58:50 +0900

    Update axes dynamically

--- src/dataStore.js
+++ src/dataStore.js
@@ -1,14 +1,19 @@
+const MIN_X = 500;
+const MAX_X = 2000;
+const MIN_Y = 100;
+const MAX_Y = 300;
+const MIN_SIZE = 2;
+const MAX_SIZE = 50;
+
 export default class DataStore {
 
-    constructor(width, height) {
-        this.width = width;
-        this.height = height;
+    constructor() {
         this.data = [];
     }
 
     init(count) {
         for (let i = 0; i < count; i++) {
-            this.data.push(this.randomData());
+            this.data.push(randomData());
         }
     }
 
@@ -16,39 +21,43 @@ export default class DataStore {
         return [...this.data];
     }
 
-    randomData() {
-        return {
-            id: Math.random() * 10000,
-            x: Math.random() * this.width,
-            y: Math.random() * this.height,
-            size: 2 + Math.random() * 50,
-            selected: false
-        };
-    }
-
     randomUpdate() {
         const r = Math.random();
         if (r < 0.1) {
             console.log("Adding new bubbles");
-            this.data.push(this.randomData());
+            this.data.push(randomData());
         } else if (this.data.length > 0) {
             const index = Math.floor(Math.random() * this.data.length);
             if (r < 0.2) {
                 console.log(`Deleting bubble at index ${index}`);
                 this.data.splice(index, 1);
             } else if (r < 0.3) {
-                const newSize = this.data[index].size * (0.5 + Math.random());
+                const newSize = normalize(this.data[index].size * (0.5 + Math.random()), MAX_SIZE, MIN_SIZE);
                 console.log(`Updating size of bubble at index ${index} to ${newSize}`);
                 this.data[index].size = newSize;
             } else if (r < 0.4) {
-                const newX = this.data[index].x * (0.5 + Math.random());
+                const newX = normalize(this.data[index].x * (0.5 + Math.random()), MIN_X, MAX_X);
                 console.log(`Updating X of bubble at index ${index} to ${newX}`);
                 this.data[index].x = newX;
             } else if (r < 0.5) {
-                const newY = this.data[index].y * (0.5 + Math.random());
+                const newY = normalize(this.data[index].y * (0.5 + Math.random()), MIN_Y, MAX_Y);
                 console.log(`Updating Y of bubble at index ${index} to ${newY}`);
                 this.data[index].y = newY;
             }
         }
     }
+}
+
+function randomData() {
+    return {
+        id: Math.random() * 10000,
+        x: MIN_X + Math.random() * (MAX_X - MIN_X),
+        y: MIN_Y + Math.random() * (MAX_Y - MIN_Y),
+        size: MIN_SIZE + Math.random() * (MAX_SIZE - MIN_SIZE),
+        selected: false
+    };
+}
+
+function normalize(value, min, max) {
+    return Math.max(Math.min(value, max), min);
 }
--- src/index.js
+++ src/index.js
@@ -1,26 +1,32 @@
+/* eslint-disable no-unused-vars */
+
 import * as d3 from "d3";
 import "./styles.css";
 import DataStore from "./dataStore";
 
-const WIDTH = 800;
-const HEIGHT = 800;
+const GRAPH_WIDTH = 800;
+const GRAPH_HEIGHT = 800;
 const INITIAL_BUBBLES = 100;
 const MARGIN = {top: 20, right: 20, bottom: 30, left: 30};
 
 const xScale = d3.scaleLinear()
-    .domain([0, WIDTH])
-    .range([MARGIN.left, WIDTH - MARGIN.right]);
+    .domain([0, 5000])
+    .range([MARGIN.left, GRAPH_WIDTH - MARGIN.right]);
 
 const yScale = d3.scaleLinear()
-    .domain([0, HEIGHT])
-    .range([HEIGHT - MARGIN.bottom, MARGIN.top]);
+    .domain([0, 5000])
+    .range([GRAPH_HEIGHT - MARGIN.bottom, MARGIN.top]);
+
+// var colorScale = d3.scaleLinear()
+//     .domain([-1, 0, 1])
+//     .range(["red", "white", "green"]);
 
 const svg = d3.select("#root")
     .append("svg")
-    .attr("width", WIDTH)
-    .attr("height", HEIGHT);
+    .attr("width", GRAPH_WIDTH)
+    .attr("height", GRAPH_HEIGHT);
 
-const dataStore = new DataStore(WIDTH, HEIGHT);
+const dataStore = new DataStore();
 dataStore.init(INITIAL_BUBBLES);
 
 const selectionBrush = d3.brush()
@@ -48,11 +54,41 @@ function flagSelected(data, left, top, right, bottom) {
     });
 }
 
+function adjustScale(scale, data, getter) {
+    let min = null, max = null;
+    data.forEach(d => {
+        const v = getter(d);
+        if (min === null || v < min) {
+            min = v;
+        }
+        if (max === null || v > max) {
+            max = v;
+        }
+    });
+    if (min !== null && max !== null) {
+        const margin = (max - min) / 10;
+        scale.domain([min - margin, max + margin]);
+    }
+}
+
+function refresh() {
+    const newData = dataStore.getData();
+
+    adjustScale(xScale, newData, d => d.x);
+    adjustScale(yScale, newData, d => d.y);
+
+    renderBubbles();
+
+    // In a real life scenario, we should only update the scale/axes in extreme cases
+    renderAxes();
+}
+
 function renderBubbles() {
     // Sets the data in the selection
     const newData = dataStore.getData();
     pointSelection = pointSelection.data(newData, d => d.id);
 
+    // Update current elements
     pointSelection
         .attr("cx", d => xScale(d.x))
         .attr("cy", d => yScale(d.y))
@@ -72,25 +108,28 @@ function renderBubbles() {
     pointSelection = newSelection.merge(pointSelection);
 }
 
-const xAxis = d3.axisBottom(xScale);
-const yAxis = d3.axisLeft(yScale);
+function renderAxes() {
+    d3.select(".axisLayer").remove();
 
-const axisLayer = svg.append("g")
-    .attr("class", "axisLayer");
+    const axisLayer = svg.append("g")
+        .attr("class", "axisLayer");
 
-axisLayer.append("g")
-    .attr("transform", `translate(0, ${HEIGHT - MARGIN.bottom})`)
-    .call(xAxis);
+    const xAxis = d3.axisBottom(xScale);
+    axisLayer.append("g")
+        .attr("transform", `translate(0, ${GRAPH_HEIGHT - MARGIN.bottom})`)
+        .call(xAxis);
 
-axisLayer.append("g")
-    .attr("transform", `translate(${MARGIN.left}, 0)`)
-    .call(yAxis);
+    const yAxis = d3.axisLeft(yScale);
+    axisLayer.append("g")
+        .attr("transform", `translate(${MARGIN.left}, 0)`)
+        .call(yAxis);
+}
 
 // Initial view
-renderBubbles();
+refresh();
 
 // Random updates
 setInterval(() => {
     dataStore.randomUpdate();
-    renderBubbles();
+    refresh();
 }, 250);
