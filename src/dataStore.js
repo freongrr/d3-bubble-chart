ea42975d216f7e70d927c21f84a1656b2630ef18
Fabien Cortina <fabien.cortina@gmail.com>
2019-01-02 20:59:31 +0900

    Remove useless logging

--- src/dataStore.js
+++ src/dataStore.js
@@ -26,29 +26,19 @@ export default class DataStore {
     randomUpdate() {
         const r = Math.random();
         if (r < 0.1) {
-            // console.log("Adding new bubbles");
             this.data.push(randomData());
         } else if (this.data.length > 0) {
             const index = Math.floor(Math.random() * this.data.length);
             if (r < 0.2) {
-                // console.log(`Deleting bubble at index ${index}`);
                 this.data.splice(index, 1);
             } else if (r < 0.3) {
-                const newSize = normalize(this.data[index].size * (0.5 + Math.random()), MAX_SIZE, MIN_SIZE);
-                // console.log(`Updating size of bubble at index ${index} to ${newSize}`);
-                this.data[index].size = newSize;
+                this.data[index].size = normalize(this.data[index].size * (0.5 + Math.random()), MAX_SIZE, MIN_SIZE);
             } else if (r < 0.4) {
-                const newX = normalize(this.data[index].x * (0.5 + Math.random()), MIN_X, MAX_X);
-                // console.log(`Updating X of bubble at index ${index} to ${newX}`);
-                this.data[index].x = newX;
+                this.data[index].x = normalize(this.data[index].x * (0.5 + Math.random()), MIN_X, MAX_X);
             } else if (r < 0.5) {
-                const newY = normalize(this.data[index].y * (0.5 + Math.random()), MIN_Y, MAX_Y);
-                // console.log(`Updating Y of bubble at index ${index} to ${newY}`);
-                this.data[index].y = newY;
+                this.data[index].y = normalize(this.data[index].y * (0.5 + Math.random()), MIN_Y, MAX_Y);
             } else if (r < 0.6) {
-                const newZ = normalize(this.data[index].z * (0.5 + Math.random()), MIN_Z, MAX_Z);
-                // console.log(`Updating Z of bubble at index ${index} to ${newZ}`);
-                this.data[index].z = newZ;
+                this.data[index].z = normalize(this.data[index].z * (0.5 + Math.random()), MIN_Z, MAX_Z);
             }
         }
     }
