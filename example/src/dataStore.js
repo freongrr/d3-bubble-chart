a507289675fe9b1d1edccd1b1ec0120f36a2ac12
Fabien Cortina <fabien.cortina@gmail.com>
2019-01-10 14:23:03 +0900

    Set a default domain when all values are 0

--- example/src/dataStore.js
+++ example/src/dataStore.js
@@ -1,6 +1,6 @@
 const MIN_X = 500;
 const MAX_X = 2000;
-const MIN_Y = 100;
+const MIN_Y = -100;
 const MAX_Y = 300;
 const MIN_Z = 0;
 const MAX_Z = 100;
@@ -32,24 +32,32 @@ export default class DataStore {
             if (r < 0.2) {
                 this.data.splice(index, 1);
             } else {
-                this.randomUpdateValue(this.data[index]);
+                DataStore.randomUpdateValue(this.data[index]);
             }
         }
     }
 
-    randomUpdateValue(d) {
+    static randomUpdateValue(d) {
         const r = Math.random();
         if (r < 0.1) {
-            d.size = normalize(d.size * (0.5 + Math.random()), MAX_SIZE, MIN_SIZE);
+            d.size = DataStore.randomValueCloseTo(d.size, MIN_SIZE, MAX_SIZE);
         }
         if (r < 0.2) {
-            d.x = normalize(d.x * (0.5 + Math.random()), MIN_X, MAX_X);
+            d.x = DataStore.randomValueCloseTo(d.x, MIN_X, MAX_X);
         }
         if (r < 0.3) {
-            d.y = normalize(d.y * (0.5 + Math.random()), MIN_Y, MAX_Y);
+            d.y = DataStore.randomValueCloseTo(d.y, MIN_Y, MAX_Y);
         }
         if (r < 0.4) {
-            d.z = normalize(d.z * (0.5 + Math.random()), MIN_Z, MAX_Z);
+            d.z = DataStore.randomValueCloseTo(d.z, MIN_Z, MAX_Z);
+        }
+    }
+
+    static randomValueCloseTo(value, min, max) {
+        if (value === 0) {
+            return min + (max - min) * Math.random();
+        } else {
+            return normalize(value * (0.5 + Math.random()), min, max);
         }
     }
 }
--- src/autoScale.js
+++ src/autoScale.js
@@ -12,8 +12,15 @@ export function adjustScale(scale, data, getter, options) {
     const [oldMin, oldMax] = getCurrentExtent(scale, options);
     const [newMin, newMax] = getExtent(oldMin, oldMax, min, max, options);
     if (newMin !== oldMin || newMax !== oldMax) {
-        const margin = max === min ? (max * options.marginRatio) : (max - min) * options.marginRatio;
-        scale.domain([min - margin, max + margin]);
+        if (newMin === 0 && newMax === 0) {
+            scale.domain([-1, 1]);
+        } else if (max === min) {
+            const margin = max * options.marginRatio;
+            scale.domain([min - margin, max + margin]);
+        } else {
+            const margin = (max - min) * options.marginRatio;
+            scale.domain([min - margin, max + margin]);
+        }
         return true;
     } else {
         return false;
@@ -49,32 +56,24 @@ export function getExtent(oldMin, oldMax, newMin, newMax, options) {
     const extent = [oldMin, oldMax];
     if (newMin < oldMin) {
         const ratio = (oldMin - newMin) / oldRange;
-        // console.log(`Min bound extended: ${oldMin} -> ${newMin}, ratio=${ratio}`);
         if (ratio > options.extendThreshold) {
-            // console.log("Keeping new (lower) min");
             extent[0] = newMin;
         }
     } else if (newMin > oldMin) {
         const ratio = (newMin - oldMin) / oldRange;
-        // console.log(`Min bound contracted: ${oldMin} -> ${newMin}, ratio=${ratio}`);
         if (ratio > options.contractThreshold) {
-            // console.log("Keeping new (higher) min");
             extent[0] = newMin;
         }
     }
 
     if (newMax > oldMax) {
         const ratio = (newMax - oldMax) / oldRange;
-        // console.log(`Max bound extended: ${oldMax} -> ${newMax}, ratio=${ratio}`);
         if (ratio > options.extendThreshold) {
-            // console.log("Keeping new (higher) max");
             extent[1] = newMax;
         }
     } else if (newMax < oldMax) {
         const ratio = (oldMax - newMax) / oldRange;
-        // console.log(`Min bound contracted: ${oldMin} -> ${newMin}, ratio=${ratio}`);
         if (ratio > options.contractThreshold) {
-            // console.log("Keeping new (lower) max");
             extent[1] = newMax;
         }
     }
